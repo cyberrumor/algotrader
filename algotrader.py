b2db89cab5659c633eb7f4d99d9dbdbb4e62c2e5
cyberrumor <cyberrumor@gmail.com>
2020-07-25 18:53:13 -0600

    added moving mean and std

--- algotrader.py
+++ algotrader.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-import yfinance as yf
 
 # get some data, preferably from somewhere free like yahoo finance
 # the below is the 50 day closing prices of AMD taken on July 25th
+
 somenumbers = [54.51, 54.2,  54.59, 55.47, 56.39, 54.65, 55.17, 53.19, 52.74, 51.74, 53.8,  53.63,
  53.54, 52.73, 52.63, 53.1,  52.97, 56.39, 57.44, 52.83, 53.5,  54.68, 54.46, 54.55,
  54.04, 54.23, 54.76, 53.99, 52.39, 51.93, 50.1,  50.28, 52.61, 52.58, 52.34, 53.4,
  52.93, 53.43, 57.26, 55.88, 53.59, 54.72, 55.34, 54.92, 55.04, 57.46, 57.,   61.79,
  59.57, 69.4]
-
-amd = yf.Ticker("AMD")
-amdhist = amd.history(period="200d")
-
-
-somenumbers[1 + i:x + i]
-
-
 
 # square root formula, so we don't need to import math
 def sqrt(x):
@@ -44,41 +36,71 @@ def std(x):
 		z += i
 	return sqrt(z / len(x))
 
-average = mean(somenumbers)
-standard_deviation = std(somenumbers)
-buy = average - standard_deviation
-sell = average + standard_deviation
-
-
-def movingstd(x, y):
+# moving mean, x is the list, y is the number of data points at a time
+def movingmean(x, y):
 	e = 0
+	i = 0
 	result = []
+	while i < y:
+		result.append("nan")
+		i += 1
 	for i in x[0 + e:y + e]:
 		while e + y < len(x):
-			result[y + e] = std(x[0 + e:y + e])
-			e++
-	print(result)
+			result.append(mean(x[0 + e:y + e]))
+			e += 1
 	return result
 
+# moving standard deviation, x is the list, y is the number of data points at a time
+def movingstd(x, y):
+	e = 0
+	i = 0
+	result = []
+	while i < y:
+		result.append("nan")
+		i += 1
 
-print()
-print("data set:")
-print(somenumbers)
-print()
-print("mean:")
-print(average)
-print()
-print("standard deviation:")
-print(standard_deviation)
-print()
-print("buy below:")
-print(buy)
-print()
-print("sell above:")
-print(sell)
-print()
+	for i in x[0 + e:y + e]:
+		while e + y < len(x):
+			result.append(std(x[0 + e:y + e]))
+			e += 1
+	return result
+
+# add list, can handle retain nan
+def addlist(x, y):
+	result = []
+	i = 0
+	while i < len(x):
+		if type(x[i]) and type(y[i]) != str:
+			result.append(x[i] + y[i])
+		else:
+			result.append("nan")
+		i += 1
+	return result
+
+# subtract list, can retain nan
+def sublist(x, y):
+	result = []
+	i = 0
+	while i < len(x):
+		if type(x[i]) and type(y[i]) != str:
+			result.append(x[i] - y[i])
+		else:
+			result.append("nan")
+		i += 1
+	return result
 
 listmovingstd = movingstd(somenumbers, 10)
-print("listmovingstd: ")
-print(movingstd)
+movingstdhigh = addlist(somenumbers, listmovingstd)
+movingstdlow = sublist(somenumbers, listmovingstd)
+listmovingmean = movingmean(somenumbers, 10)
 
+
+print("Moving 10 day std high:")
+print(movingstdhigh)
+print()
+print("Moving 10 day std low:")
+print(movingstdlow)
+print()
+print("Moving 10 day mean:")
+print(listmovingmean)
+print()
