25fbbe6ac409f628bdc5e1ca0a08923c2a673583
cyberrumor <cyberrumor@gmail.com>
2020-08-23 14:30:37 -0600

    nvm, they were broken ;)

--- nptrader.py
+++ nptrader.py
@@ -6,34 +6,30 @@ import matplotlib.pyplot as plt
 import sys
 
 shorttime = 20
-longtime = "3y"
+longtime = '3y'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	if len(sys.argv) == 1:
-		print("Usage: ./algotrader amd aapl msft nvda")
+		print('Usage: ./algotrader amd aapl msft nvda')
 		exit()
 
-	fig, axes = plt.subplots(len(sys.argv[1:]), 1, figsize=(10, 4))
+	fig, axes = plt.subplots(len(sys.argv[1:]), 1, figsize=(10, 4), sharex='col')
 
 	for stock in sys.argv[1:]:
-		print("plotting " + stock)
+		print('plotting ' + stock)
 
-
-
-		# select the subplot(columns, rows, index of selected plot)
-
-		if sys.argv.index(stock) > 1:
-			plt.subplot(len(sys.argv[1:]), 1, sys.argv.index(stock), sharex = plt.subplot(len(sys.argv[1:]), 1, sys.argv.index(stock) - 1))
-		else:
-			plt.subplot(len(sys.argv[1:]), 1, sys.argv.index(stock))
-
+		# select the subplot(columns, rows, index of selected plot).
+		plt.subplot(len(sys.argv[1:]), 1, sys.argv.index(stock))
 
 		plt.title(stock)
+
+		# Get the data
 		data = yfinance.Ticker(stock).history(period=longtime)['Close']
 		x = data.index
 		y = data.values
-		plt.plot(x, y)
-
+		plt.plot(x, y, color='black', label=stock)
+		plt.legend()
+		plt.grid(True)
 
 	plt.show()
