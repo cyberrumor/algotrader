29a7d71a1ad409a26451284b824aef295efa5cc7
cyberrumor <cyberrumor@gmail.com>
2020-08-24 10:34:56 -0600

    working sharex (for real this time)

--- nptrader.py
+++ nptrader.py
@@ -8,28 +8,20 @@ import sys
 shorttime = 20
 longtime = '3y'
 
-
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
 		print('Usage: ./algotrader amd aapl msft nvda')
 		exit()
 
-	fig, axes = plt.subplots(len(sys.argv[1:]), 1, figsize=(10, 4), sharex='col')
+	fig, axs = plt.subplots(len(sys.argv[1:]), 1, figsize=(10,4), sharex='col')
 
-	for stock in sys.argv[1:]:
-		print('plotting ' + stock)
-
-		# select the subplot(columns, rows, index of selected plot).
-		plt.subplot(len(sys.argv[1:]), 1, sys.argv.index(stock))
-
-		plt.title(stock)
-
-		# Get the data
+	for ax, stock in zip(axs, sys.argv[1:]):
+		ax.set_title(stock)
 		data = yfinance.Ticker(stock).history(period=longtime)['Close']
 		x = data.index
 		y = data.values
-		plt.plot(x, y, color='black', label=stock)
-		plt.legend()
-		plt.grid(True)
+		ax.plot(x, y, color='black', label=stock)
+		ax.legend()
+		ax.grid(True)
 
 	plt.show()
